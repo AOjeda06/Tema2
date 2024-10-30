543275db1ab7d1abd563cb65817b2fa5325bea7b
AOjeda06 <andres.ojeda@iesnervion.es>
2024-10-30 10:56:40 +0100

    30/10/2024

--- Tema 2/src/anteriores/Ej4.java
+++ Tema 2/src/anteriores/Ej4.java
@@ -1,10 +1,64 @@
 package anteriores;
 
+import java.util.Scanner;
+
 public class Ej4 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Configuramos el scanner
+		Scanner scanner = new Scanner(System.in);
 
+		// Variables
+		int lado;
+		int contador;
+		int lado1;
+		int suma;
+		int espacio;
+
+		// Pide el valor y lo guarda
+		System.out.println("¿Cuál será la proporción del hexágono?: ");
+		lado = scanner.nextInt();
+		contador = 1;
+		suma = 0;
+		espacio = lado - 1;
+
+		// Bucle que se repetirá "lado" veces
+		for (int i = 1; i <= ((lado * 2) - 1); i++) {
+			lado1 = (lado + suma);
+
+			if (contador < lado) {
+				for (int j = espacio; j > 0; j--) {
+					System.out.print(" ");
+				}
+				for (int k = 1; k <= lado1; k++) {
+					System.out.print("*");
+				}
+				contador++;
+				suma += 2;
+				espacio--;
+				System.out.println();
+			} else if (contador == lado) {
+				for (int k = 1; k <= lado1; k++) {
+					System.out.print("*");
+				}
+				contador++;
+				suma -= 2; // Empieza a decrementar aquí para preparar la próxima línea
+				espacio++;
+				System.out.println();
+			} else {
+				for (int j = espacio; j > 0; j--) {
+					System.out.print(" ");
+				}
+				for (int k = 1; k <= lado1; k++) {
+					System.out.print("*");
+				}
+				contador++;
+				suma -= 2;
+				espacio++;
+				System.out.println();
+			}
+		}
+
+		// Cerramos el scanner
+		scanner.close();
 	}
-
 }
